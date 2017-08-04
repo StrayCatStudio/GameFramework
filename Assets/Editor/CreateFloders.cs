3fd5d7328c39535a489ee9cee32d60d6d37d3d57
LuoRui <roget_lr@126.com>
2017-08-04 12:25:08 +0800

    Revert "Revert "Add Folders""
    This reverts commit d78f2976f1fd1105a339fc84ff0800380f5dc135.

---
+++ Assets/Editor/CreateFloders.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+/// <summary>
+/// 一键创建项目文件夹
+/// </summary>
+public class CreateFloders : MonoBehaviour
+{
+#if UNITY_EDITOR
+    /// <summary>
+    /// 创建项目文件夹
+    /// </summary>
+    [MenuItem("Tools/CreateFloders #&_b")]
+    public static void ToCreateFloders()
+    {
+        string _path = Application.dataPath;
+        Directory.CreateDirectory(Path.Combine(_path, "AssetBundles"));
+        Directory.CreateDirectory(Path.Combine(_path, "Editor"));
+        Directory.CreateDirectory(Path.Combine(_path, "Plugins"));
+        Directory.CreateDirectory(Path.Combine(_path, "Prefabs"));
+        Directory.CreateDirectory(Path.Combine(_path, "Resources"));
+        Directory.CreateDirectory(Path.Combine(_path, "Scenes"));
+        Directory.CreateDirectory(Path.Combine(_path, "Scripts"));
+        Directory.CreateDirectory(Path.Combine(_path, "StreamingAssets"));
+
+        _path = Path.Combine(Application.dataPath, "ToBeAssetBundle");
+        Directory.CreateDirectory(Path.Combine(_path, "Atlas"));
+        Directory.CreateDirectory(Path.Combine(_path, "Audios"));
+        Directory.CreateDirectory(Path.Combine(_path, "GUI"));
+        Directory.CreateDirectory(Path.Combine(_path, "Materials"));
+        Directory.CreateDirectory(Path.Combine(_path, "Models"));
+        Directory.CreateDirectory(Path.Combine(_path, "Others"));
+        Directory.CreateDirectory(Path.Combine(_path, "Shader"));
+        Directory.CreateDirectory(Path.Combine(_path, "Textures"));
+        Directory.CreateDirectory(Path.Combine(_path, "Xmls"));
+
+        AssetDatabase.Refresh();
+
+        Debug.Log("项目文件夹创建完成!");
+    }
+#endif
+}
