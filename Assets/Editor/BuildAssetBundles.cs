1a008f41fc2cf2e328afa0ff223018512567122a
LuoRui <roget_lr@126.com>
2017-09-06 18:42:40 +0800

    Update files

---
+++ Assets/Editor/BuildAssetBundles.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class BuildAssetBundles : MonoBehaviour
+{
+    /// <summary>
+    /// 设置资源包名称
+    /// </summary>
+    [MenuItem("Tools/SetAssetBundleName")]
+    public void SetAssetBundleName()
+    {
+        string _path = Application.dataPath;
+
+    }
+}
